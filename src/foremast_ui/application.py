63d9c6bc16f04e083faae4c6fae71a0dfc1566b9
Nate Tangsurat <e4r7hbug@gmail.com>
2017-08-19 16:44:06 -0500

    docs: Update OpenAPI information

--- src/foremast_ui/application.py
+++ src/foremast_ui/application.py
@@ -1,6 +1,9 @@
 """Main application."""
 import connexion
+from pkg_resources import get_distribution
 
-
-APP = connexion.App(__package__, specification_dir='openapi')
+APP = connexion.App(
+    __package__, specification_dir='openapi', arguments={
+        'foremast_version': get_distribution('foremast').version,
+    })
 APP.add_api('v1.yml', resolver=connexion.resolver.RestyResolver('.'.join([__package__, 'api.v1'])))

cb0ace4fd48b259a3051342db85e88267d0d8583
Nate Tangsurat <e4r7hbug@gmail.com>
2017-08-19 14:56:11 -0500

    fix: Use read module path

--- src/foremast_ui/application.py
+++ src/foremast_ui/application.py
@@ -3,4 +3,4 @@ import connexion
 
 
 APP = connexion.App(__package__, specification_dir='openapi')
-APP.add_api('v1.yml', resolver=connexion.resolver.RestyResolver('api'))
+APP.add_api('v1.yml', resolver=connexion.resolver.RestyResolver('.'.join([__package__, 'api.v1'])))

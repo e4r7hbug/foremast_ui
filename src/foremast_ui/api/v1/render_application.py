613d0b978f1353f02abb1b0e4ce500ef453d9ace
Nate Tangsurat <e4r7hbug@gmail.com>
2017-08-19 15:37:41 -0500

    feat: Get external arguments for application.json

--- src/foremast_ui/api/v1/render_application.py
+++ src/foremast_ui/api/v1/render_application.py
@@ -1,6 +1,18 @@
 """Render application.json configuration."""
+import json
+
+import gogoutils
+from foremast.consts import APP_FORMATS
+from foremast.utils import DeepChainMap, get_template
 
 
-def post(application_json):
+def post(env, git_uri, application_json):
     """Render application.json."""
-    return application_json
+    generated = gogoutils.Generator(*gogoutils.Parser(git_uri).parse_url(), formats=APP_FORMATS)
+
+    app_name = generated.app_name()
+    profile = generated.iam()['profile']
+
+    rendered_template = json.loads(get_template('configs/configs.json.j2', env=env, app=app_name, profile=profile))
+    rendered = dict(DeepChainMap(application_json, rendered_template))
+    return rendered

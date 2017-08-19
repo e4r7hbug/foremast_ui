f8319ce835e22f56150328c3f28f6fbb53e94830
Nate Tangsurat <e4r7hbug@gmail.com>
2017-08-19 18:03:15 -0500

    feat: Render defaults into index template

--- src/foremast_ui/routes/index.py
+++ src/foremast_ui/routes/index.py
@@ -1,8 +1,15 @@
 """Site root."""
+import flask
+
+from ..api.v1.render_application import post
 from ..application import APP
 
 
 @APP.route('/')
 def index():
     """Serve index page."""
-    return 'This is a test'
+    default_application_json = post('test', 'group/project', {})
+
+    rendered = flask.render_template('index.html.j2', application_json=default_application_json)
+
+    return rendered

c5e7149f9a3b28ecebb9634c38032b460677dfa0
Nate Tangsurat <e4r7hbug@gmail.com>
2017-08-19 03:15:18 -0500

    feat: Make pip installable

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""Install Foremast Lathe."""
+from setuptools import find_packages, setup
+
+with open('README.rst') as readme_handle:
+    LONG_DESCRIPTION = readme_handle.read()
+
+setup(
+    name='foremast ui',
+    description='Foremast configuration generator web UI.',
+    long_description=LONG_DESCRIPTION,
+    author='',
+    author_email='',
+    install_requires=[
+        'click',
+        'falsy',
+        'foremast',
+        'gunicorn',
+    ],
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    setup_requires=['setuptools_scm'],
+    use_scm_version={'local_scheme': 'dirty-tag'},
+    entry_points={
+        'console_scripts': [
+            'foremast_ui=foremast_ui.__main__:main',
+        ],
+    }, )
